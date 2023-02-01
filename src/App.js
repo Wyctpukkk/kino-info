2b422100e6a07baf6e490f39c25d9a6074cba1c3
Vitaly <poroshenkoe@gmail.com>
2023-02-01 23:33:49 +0300

    first commit: initial, delete trash, add html+scss structure of header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logo from './assets/img/logo.svg';
+import search from './assets/img/search.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="header">
+          <a className="header__logo">
+            <img src={logo} alt="x" />
+          </a>
+          <h2>Kallyas</h2>
+          <ul className="header__navlist">
+            <li className="header__item">
+              <a className="header__text">Homepage</a>
+            </li>
+            <li className="header__item">
+              <a className="header__text">Serials</a>
+            </li>
+            <li className="header__item">
+              <a className="header__text">Movies</a>
+            </li>
+            <li className="header__item">
+              <a className="header__text">Favorites</a>
+            </li>
+            <li className="header__item">
+              <a className="header__text">My List</a>
+            </li>
+          </ul>
+          <div className="input">
+            <input className="input__field" placeholder="Search..."></input>
+            <img className="input__search" src={search} alt="search" />
+          </div>
+          <button className="header__login">Login</button>
+          <button className="header__sign">Sign Up</button>
+        </header>
+      </div>
     </div>
   );
 }
