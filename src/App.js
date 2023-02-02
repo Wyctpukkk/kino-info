29a903cb68542d2c0243e9c4b8b3121302ee5b08
Vitaly <poroshenkoe@gmail.com>
2023-02-02 05:44:23 +0300

    add: slider structure html + scss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import search from './assets/img/search.svg';
 import avengers from './assets/img/theavenger.jpg';
 import plus from './assets/img/plus.svg';
 import play from './assets/img/play-button.svg';
+import slider from './assets/img/slider.jpg';
+import leftArrow from './assets/img/left_arrow.svg';
+import rightArrow from './assets/img/right_arrow.svg';
 
 function App() {
   return (
@@ -15,7 +18,6 @@ function App() {
               <img src={logo} alt="logo" />
               <h2>Kallyas</h2>
             </div>
-
             <ul className="header__navlist">
               <li className="header__item">
                 <a className="header__text">Homepage</a>
@@ -41,44 +43,75 @@ function App() {
             <button className="header__sign">Sign Up</button>
           </header>
         </div>
-
         <div className="container">
-          <main>
-            <section className="first-block">
-              <article className="first-left">
-                <div className="first-left__category">
-                  <button className="first-left__category__year">2012</button>
-                  <button className="first-left__category__type">action</button>
-                  <button className="first-left__category__type">
-                    adventure
-                  </button>
-                </div>
-                <h2>the avengers</h2>
-                <p>
-                  When Thor's evil brother, Loki (Tom Hiddleston), gains access
-                  to the unlimited power of the energy cube called the
-                  Tesseract, Nick Fury (Samuel L. Jackson), director of
-                  S.H.I.E.L.D., initiates a superhero recruitment effort to
-                  defeat the unprecedented threat to Earth.
-                </p>
-                <div className="first-left__buttons">
-                  <button className="first-left__buttons__play">
-                    Play trailer
-                    <img src={play} alt="play" />
-                  </button>
-                  <button className="first-left__buttons__add">
-                    add to list
-                    <img src={plus} alt="plus" />
-                  </button>
-                </div>
-              </article>
-              <article className="first-right">
-                <img src={avengers} alt="poster" />
-              </article>
-            </section>
-          </main>
+          <section className="first-block">
+            <article className="first-left">
+              <div className="first-left__category">
+                <button className="first-left__category__year">2012</button>
+                <button className="first-left__category__type">action</button>
+                <button className="first-left__category__type">
+                  adventure
+                </button>
+              </div>
+              <h2>the avengers</h2>
+              <p>
+                When Thor's evil brother, Loki (Tom Hiddleston), gains access to
+                the unlimited power of the energy cube called the Tesseract,
+                Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D.,
+                initiates a superhero recruitment effort to defeat the
+                unprecedented threat to Earth.
+              </p>
+              <div className="first-left__buttons">
+                <button className="first-left__buttons__play">
+                  Play trailer
+                  <img src={play} alt="play" />
+                </button>
+                <button className="first-left__buttons__add">
+                  add to list
+                  <img src={plus} alt="plus" />
+                </button>
+              </div>
+            </article>
+            <article className="first-right">
+              <img src={avengers} alt="poster" />
+            </article>
+          </section>
         </div>
       </div>
+      <section className="slider">
+        <h2 className="slider__title">Popular serials</h2>
+        <article className="slider__block">
+          <button className="slider__back">
+            <img src={leftArrow} />
+          </button>
+          <ul className="slider__list">
+            <li className="slider__item first">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item">
+              <img className="slider__content" src={slider} />
+            </li>
+            <li className="slider__item last">
+              <img className="slider__content" src={slider} />
+            </li>
+          </ul>
+          <button className="slider__next">
+            <img src={rightArrow} />
+          </button>
+        </article>
+      </section>
     </div>
   );
 }
